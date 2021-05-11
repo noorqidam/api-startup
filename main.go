63cbdf39fcf8d8c452172ebdc583bed6ba475107
noorqidam <noorqidam@gmail.com>
2021-05-11 14:49:49 +0700

    Endpoint Email Checking

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
+	api.POST("/email-checkers", userHandler.CheckEmailAvailability)
 	router.Run()
 
 	// userInput := user.RegisterUserInput{}
